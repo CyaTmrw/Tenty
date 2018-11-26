c9c4605d6dbe514bfb2cc650af16d77cc51af5f0
root <root@hw5.localdomain>
2018-11-26 18:13:46 +0000

    updated about page

--- src/About.js
+++ src/About.js
@@ -9,25 +9,24 @@ class About extends Component {
         <div className="About">
             <Header />
             <div className="aboutContent">
-                <h1>How does phone scams work?</h1>
+                <h1>How do phone scams work?</h1>
                 <p>
-                    First, CRA scam starts off a robocall claiming to be CRA and telling the recipient that they owe a huge amount of unpaid taxes.
+                    A CRA scam starts with a robocall claiming to be from Canadian Revenue Agency.They tell the recipient that they owe a huge amount in unpaid taxes.
                     Then, the recipient will be informed that they must call back or they will be arrested and put in prison.
                     If the recipien does call back, he or she subjected to further threats and offered a one time offer to settle the matter by paying the money that they “own”.
                 </p>
                 <h1>What are we trying to accomplish?</h1>
                 <p>
-                    Stop scammers by making it unsustainable for them to scam people. We want to both warn the user and block scam calls.
-                    Eventually, there will be less scammer due to less people will be scammed .
+                    We want to prevent fraud from taking place. Specifically protecting those that are vulnerable. By gathering lists of scammers we are able to produce blacklists for telecommunications companies and as well the Authorities.
+               
                 </p>
-                <h1>How would going to accomplish it?</h1>
+                <h1>How is this accomplished?</h1>
                 <p>
-                    We have created an app that warns the users about calls that are suspected to be from scam number.
-                    Whenever person receives a call from a scammer, the client will send the number to server to check if it is scam number.
+                    We have created an app that warns users about calls and SMS that are suspected to be from a scam number.
+                    If a number calls the phone and is not a Contact the application will send the number to a server to check if it is a recognized scam number.
                     If it is a scam number, the app will warn the user with a notification.
-                    If a user did not get a warning from the app, the person can report the number.
-                    When a lot of people have reported the same number, the number will be added the blacklist.
-                    We also records the call and analysis the audio to see if it is a robocall.
+                    If a user did not get a warning from the app, but it was a scam number the person can report the number.
+                    When a lot of people have reported the same number or it was previously classified, the number will be added to the blacklist.
                 </p>
                 <h1>How did we get this idea?</h1>
                 <p>
